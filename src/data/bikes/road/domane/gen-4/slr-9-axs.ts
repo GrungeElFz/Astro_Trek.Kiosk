f7aabcb12b584419333f44d1df02dd4a2804eb4d
GrungeElFz <gdelviora@0x01000111.io>
2025-08-19 18:28:55 -0500

    Added:    Product data for `Domane SLR 9 AXS` (src/data/bikes/road/domane/gen-4/slr-9-axs.ts)

---
+++ src/data/bikes/road/domane/gen-4/slr-9-axs.ts
@@ -0,0 +1,354 @@
+import type { Model } from "@/data/types";
+
+export const domaneSLR9AXS: Model = {
+  modelName: "Domane SLR 9 AXS",
+  overview:
+    "Our top-of-the-line, ultralight carbon endurance road bike decked out with the best parts for a fast and high-performance ride.",
+  colors: [
+    {
+      name: "Matte Deep Smoke",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/DomaneSLR9AXS-26-57944-A-Primary",
+    },
+    {
+      name: "Gecko Phaze",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/DomaneSLR9AXS-26-57944-C-Primary",
+    },
+    {
+      name: "Metallic Red Smoke / Red Carbon Smoke",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/DomaneSLR9AXS-26-57944-D-Primary",
+    },
+    {
+      name: "Era White / Supernova Marble",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/DomaneSLR9AXS-26-57944-B-Primary",
+    },
+  ],
+
+  variants: [
+    // <----- Variant: Matte Deep Smoke ----->
+    {
+      sku: "5338461",
+      size: "47",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338462",
+      size: "50",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5339363",
+      size: "52",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338464",
+      size: "54",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338465",
+      size: "56",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338466",
+      size: "58",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338467",
+      size: "60",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338468",
+      size: "62",
+      colorName: "Matte Deep Smoke",
+      price: 12499.99,
+    },
+
+    // <----- Variant: Gecko Phaze ----->
+    {
+      sku: "5338477",
+      size: "47",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+    {
+      sku: "5338478",
+      size: "50",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+    {
+      sku: "5338479",
+      size: "52",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+    {
+      sku: "5338480",
+      size: "54",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+    {
+      sku: "5338481",
+      size: "56",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+    {
+      sku: "5338482",
+      size: "58",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+    {
+      sku: "5338483",
+      size: "60",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+    {
+      sku: "5338484",
+      size: "62",
+      colorName: "Gecko Phaze",
+      price: 12499.99,
+    },
+
+    // <----- Variant: Metallic Red Smoke / Red Carbon Smoke ----->
+    {
+      sku: "5338485",
+      size: "47",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338486",
+      size: "50",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338487",
+      size: "52",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338488",
+      size: "54",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338489",
+      size: "56",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338490",
+      size: "58",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338491",
+      size: "60",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+    {
+      sku: "5338492",
+      size: "62",
+      colorName: "Metallic Red Smoke / Red Carbon Smoke",
+      price: 12499.99,
+    },
+
+    // <----- Variant: Era White / Supernova Marble ----->
+    {
+      sku: "5338469",
+      size: "47",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+    {
+      sku: "5338470",
+      size: "50",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+    {
+      sku: "5338471",
+      size: "52",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+    {
+      sku: "5338472",
+      size: "54",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+    {
+      sku: "5338473",
+      size: "56",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+    {
+      sku: "5338474",
+      size: "58",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+    {
+      sku: "5338475",
+      size: "60",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+    {
+      sku: "5338476",
+      size: "62",
+      colorName: "Era White / Supernova Marble",
+      price: 12499.99,
+    },
+  ],
+
+  base_specs: {
+    frameset: {
+      Frame: "800 Series OCLV Carbon",
+      Fork: "Domane Tapered Carbon Steerer",
+    },
+    weight: {
+      "Weight Limit":
+        "125 kg / 275 lbs (Maximum Combined Weight of Bicycle, Rider, and Cargo)",
+    },
+    "e-system": {
+      Battery: "SRAM AXS battery pack (with Charger)",
+    },
+    drivetrain: {
+      Shifter: "SRAM RED AXS E1",
+      "Front Derailleur": "SRAM RED AXS E1, Braze-on",
+      "Rear Derailleur": "SRAM RED AXS E1, 36T Max Cog",
+      "Bottom Bracket": "SRAM DUB, T47 Threaded, Internal Bearing",
+      Cassette: "SRAM RED XG-1290 E1, 10-36, 12 Speed",
+      Chain: "SRAM RED E1, 12/13 Speed",
+      "Max Chainring Size": "1x: 48T, 2x: 52/36",
+    },
+    wheels: {
+      "Wheel - Front":
+        "Bontrager Aeolus RSL 51, OCLV Carbon, Tubeless Ready, 51mm Rim Depth, 100x12mm Thru Axle",
+      "Wheel - Rear":
+        "Bontrager Aeolus RSL 51, OCLV Carbon, Tubeless Ready, 51mm Rim Depth, 11/12 Freehub, 142x12mm Thru Axle",
+      "Skewer - Front": "Bontrager Switch Chamfered Thru Axle",
+      "Skewer - Rear": "Bontrager Switch Chamfered Thru Axle, Removable Lever",
+      Tire: "Bontrager Kwaremont RSL TLR, Tubeless Ready, Folding Bead, Race Dual-Compound, 320 TPI, 700x32mm",
+      "Tire Part": "Bontrager TLR sealant, 6oz",
+      "Max Tire Size": "35mm (with fender), 38mm (without fenders)",
+    },
+    components: {
+      Saddle: "Verse Short Pro, Carbon Rails, 145mm width",
+      "Handlebar Tape": "Trek EcoTack",
+      Brake: "SRAM Red AXS E1 Hydraulic Disc, Flat Mount",
+      "Brake Rotor": "SRAM Paceline X, Rounded, Centerlock, 160mm",
+      "Rotor Size": "Max Brake Rotor Sizes: 160mm",
+    },
+    accessories: {
+      Bag: "Bontrager BITS Internal Frame Storage Bag",
+    },
+  },
+
+  size_specific_specs: [
+    {
+      size: "47",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 280mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 165mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 35cm control width, 38cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 70mm length",
+      },
+    },
+    {
+      size: "50",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 280mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 165mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 37cm control width, 40cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 80mm length",
+      },
+    },
+    {
+      size: "52",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 280mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 170mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 37cm control width, 40cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 90mm length",
+      },
+    },
+    {
+      size: "54",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 280mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 170mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 39cm control width, 42cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 90mm length",
+      },
+    },
+    {
+      size: "56",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 280mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 170mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 39cm control width, 42cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 100mm length",
+      },
+    },
+    {
+      size: "58",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 320mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 172.5mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 39cm control width, 42cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 100mm length",
+      },
+    },
+    {
+      size: "60",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 320mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 172.5mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 41cm control width, 44cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 110mm length",
+      },
+    },
+    {
+      size: "62",
+      specs: {
+        Saddle: "KVF Aero Carbon Seatpost, 20mm offset, 320mm length",
+        Crank: "SRAM RED AXS E1, Power Meter, 46/33, DUB, 172.5mm length",
+        Handlebar:
+          "Bontrager Aero Pro, OCLV Carbon, 31.8mm, Di2 Routing, 80mm reach, 124mm drop, 41cm control width, 44cm drop width",
+        Stem: "Trek RCS Pro, -7 degree, 110mm length",
+      },
+    },
+  ],
+};
