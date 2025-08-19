2d9a4bdc9c86d9081134a6cafdc6ee4038cc2dfe
GrungeElFz <gdelviora@0x01000111.io>
2025-08-19 16:26:03 -0500

    Added:    Type definitions for the bike data model (src/data/types.ts) Added:    Detailed product data for `Madone SLR 9 AXS Gen 8` (src/data/bikes/road/madone/gen-8/slr-9-axs.ts)

---
+++ src/data/bikes/road/madone/gen-8/slr-9-axs.ts
@@ -0,0 +1,504 @@
+import type { Model } from "@/data/types";
+
+export const madoneSLR9AXS: Model = {
+  modelName: "SLR 9 AXS",
+  overview:
+    "The Madone SLR 9 AXS is the pinnacle of racing performance, combining our most advanced 900 Series OCLV Carbon frame with the top-tier SRAM RED AXS wireless groupset for uncompromising speed.",
+  colors: [
+    {
+      name: "Matte Carbon Smoke / Prismatic Pearl",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-A-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-A-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-A-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-A-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-A-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-A-Alt5",
+      ],
+    },
+    {
+      name: "Matte Deep Smoke",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-I-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-I-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-I-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-I-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-I-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-I-Alt5",
+      ],
+    },
+    {
+      name: "Team Replica",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-B-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-B-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-B-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-B-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-B-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-B-Alt5",
+      ],
+    },
+    {
+      name: "ICON Interstellar",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-E-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-E-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-E-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-E-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-E-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-E-Alt5",
+      ],
+    },
+    {
+      name: "Purple Phaze / Amethyst Marble",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-J-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-J-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-J-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-J-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-J-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-J-Alt5",
+      ],
+    },
+    {
+      name: "ICON Tête de la Course",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-G-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-G-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-G-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-G-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-G-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-G-Alt5",
+      ],
+    },
+    {
+      name: "Carbon Red Smoke",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-C-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-C-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-C-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-C-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-C-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-C-Alt5",
+      ],
+    },
+    {
+      name: "Era White / Supernova Marble",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-D-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-25-46151-D-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-D-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-D-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-D-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/MadoneSLR9AXS-26-46151-D-Alt5",
+      ],
+    },
+  ],
+
+  variants: [
+    // <----- Variant: Matte Carbon Smoke / Prismatic Pearl ----->
+    {
+      sku: "5319499",
+      size: "XS",
+      colorName: "Matte Carbon Smoke / Prismatic Pearl",
+      price: 13499.99,
+    },
+    {
+      sku: "5319500",
+      size: "S",
+      colorName: "Matte Carbon Smoke / Prismatic Pearl",
+      price: 13499.99,
+    },
+    {
+      sku: "5319501",
+      size: "M",
+      colorName: "Matte Carbon Smoke / Prismatic Pearl",
+      price: 13499.99,
+    },
+    {
+      sku: "5319502",
+      size: "ML",
+      colorName: "Matte Carbon Smoke / Prismatic Pearl",
+      price: 13499.99,
+    },
+    {
+      sku: "5319503",
+      size: "L",
+      colorName: "Matte Carbon Smoke / Prismatic Pearl",
+      price: 13499.99,
+    },
+    {
+      sku: "5319504",
+      size: "XL",
+      colorName: "Matte Carbon Smoke / Prismatic Pearl",
+      price: 13499.99,
+    },
+
+    // <----- Variant: Matte Deep Smoke ----->
+    {
+      sku: "5339362",
+      size: "XS",
+      colorName: "Matte Deep Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5339363",
+      size: "S",
+      colorName: "Matte Deep Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5339364",
+      size: "M",
+      colorName: "Matte Deep Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5339365",
+      size: "ML",
+      colorName: "Matte Deep Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5339366",
+      size: "L",
+      colorName: "Matte Deep Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5339367",
+      size: "XL",
+      colorName: "Matte Deep Smoke",
+      price: 13499.99,
+    },
+
+    // <----- Variant: Team Replica ----->
+    {
+      sku: "5319505",
+      size: "XS",
+      colorName: "Team Replica",
+      price: 13499.99,
+    },
+    {
+      sku: "5319506",
+      size: "S",
+      colorName: "Team Replica",
+      price: 13499.99,
+    },
+    {
+      sku: "5319507",
+      size: "M",
+      colorName: "Team Replica",
+      price: 13499.99,
+    },
+    {
+      sku: "5319508",
+      size: "ML",
+      colorName: "Team Replica",
+      price: 13499.99,
+    },
+    {
+      sku: "5319509",
+      size: "L",
+      colorName: "Team Replica",
+      price: 13499.99,
+    },
+    {
+      sku: "5319510",
+      size: "XL",
+      colorName: "Team Replica",
+      price: 13499.99,
+    },
+
+    // <----- Variant: ICON Interstellar ----->
+    {
+      sku: "5322549",
+      size: "XS",
+      colorName: "ICON Interstellar",
+      price: 16999.99,
+    },
+    {
+      sku: "5322550",
+      size: "S",
+      colorName: "ICON Interstellar",
+      price: 16999.99,
+    },
+    {
+      sku: "5322551",
+      size: "M",
+      colorName: "ICON Interstellar",
+      price: 16999.99,
+    },
+    {
+      sku: "5322552",
+      size: "ML",
+      colorName: "ICON Interstellar",
+      price: 16999.99,
+    },
+    {
+      sku: "5322553",
+      size: "L",
+      colorName: "ICON Interstellar",
+      price: 16999.99,
+    },
+    {
+      sku: "5322554",
+      size: "XL",
+      colorName: "ICON Interstellar",
+      price: 16999.99,
+    },
+
+    // <----- Variant: Purple Phaze / Amethyst Marble ----->
+    {
+      sku: "5339374",
+      size: "XS",
+      colorName: "Purple Phaze / Amethyst Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5339375",
+      size: "S",
+      colorName: "Purple Phaze / Amethyst Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5339376",
+      size: "M",
+      colorName: "Purple Phaze / Amethyst Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5339377",
+      size: "ML",
+      colorName: "Purple Phaze / Amethyst Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5339378",
+      size: "L",
+      colorName: "Purple Phaze / Amethyst Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5339379",
+      size: "XL",
+      colorName: "Purple Phaze / Amethyst Marble",
+      price: 13499.99,
+    },
+
+    // <----- Variant: ICON Tête de la Course ----->
+    {
+      sku: "5322561",
+      size: "XS",
+      colorName: "ICON Tête de la Course",
+      price: 15999.99,
+    },
+    {
+      sku: "5322562",
+      size: "S",
+      colorName: "ICON Tête de la Course",
+      price: 15999.99,
+    },
+    {
+      sku: "5322563",
+      size: "M",
+      colorName: "ICON Tête de la Course",
+      price: 15999.99,
+    },
+    {
+      sku: "5322564",
+      size: "ML",
+      colorName: "ICON Tête de la Course",
+      price: 15999.99,
+    },
+    {
+      sku: "5322565",
+      size: "L",
+      colorName: "ICON Tête de la Course",
+      price: 15999.99,
+    },
+    {
+      sku: "5322566",
+      size: "XL",
+      colorName: "ICON Tête de la Course",
+      price: 15999.99,
+    },
+
+    // <----- Variant: Carbon Red Smoke ----->
+    {
+      sku: "5319511",
+      size: "XS",
+      colorName: "Carbon Red Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5319512",
+      size: "S",
+      colorName: "Carbon Red Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5319513",
+      size: "M",
+      colorName: "Carbon Red Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5319514",
+      size: "ML",
+      colorName: "Carbon Red Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5319515",
+      size: "L",
+      colorName: "Carbon Red Smoke",
+      price: 13499.99,
+    },
+    {
+      sku: "5319516",
+      size: "XL",
+      colorName: "Carbon Red Smoke",
+      price: 13499.99,
+    },
+
+    // <----- Variant: Era White / Supernova Marble ----->
+    {
+      sku: "5319517",
+      size: "XS",
+      colorName: "Era White / Supernova Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5319518",
+      size: "S",
+      colorName: "Era White / Supernova Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5319519",
+      size: "M",
+      colorName: "Era White / Supernova Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5319520",
+      size: "ML",
+      colorName: "Era White / Supernova Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5319521",
+      size: "L",
+      colorName: "Era White / Supernova Marble",
+      price: 13499.99,
+    },
+    {
+      sku: "5319522",
+      size: "XL",
+      colorName: "Era White / Supernova Marble",
+      price: 13499.99,
+    },
+  ],
+
+  base_specs: {
+    frameset: {
+      Frame: "900 Series OCLV Carbon",
+      Fork: "Madone Gen 8 Tapered Carbon Steerer",
+    },
+    weight: {
+      Weight:
+        "ML - 7.00 kg / 15.44 lbs (with TLR Sealant, No Tubes, No Bottles/Cages)",
+      "Weight Limit":
+        "125 kg / 275 lbs (Maximum Combined Weight of Bicycle, Rider, and Cargo)",
+    },
+    "e-system": {
+      Battery: "SRAM AXS battery pack (with Charger)",
+    },
+    drivetrain: {
+      Shifter: "SRAM RED AXS E1",
+      "Front Derailleur": "SRAM RED AXS E1, Braze-on",
+      "Rear Derailleur": "SRAM RED AXS E1, 36T Max Cog",
+      "Bottom Bracket": "SRAM DUB, T47 Threaded, Internal Bearing",
+      Cassette: "SRAM RED XG-1290 E1, 10-33, 12 Speed",
+      Chain: "SRAM RED E1, 12 Speed",
+      "Max Chainring Size": "1x: 54T, 2x: 56/43",
+    },
+    wheels: {
+      "Wheel - Front":
+        "Bontrager Aeolus RSL 51, OCLV Carbon, Tubeless Ready, 51mm Rim Depth, 100x12mm Thru Axle",
+      "Wheel - Rear":
+        "Bontrager Aeolus RSL 51, OCLV Carbon, Tubeless Ready, 51mm Rim Depth, SRAM XD-R driver",
+      "Skewer - Front": "Bontrager Switch Chamfered Thru Axle",
+      "Skewer - Rear": "Bontrager Switch Chamfered Thru Axle, Removable Lever",
+      Tire: "Bontrager Aeolus RSL RD, Tubeless Ready, Cotton Construction, Aramid Bead, 170 TPI, 700x28mm",
+      "Tire Part": "Bontrager TLR sealant, 6oz",
+      Tube: "Pirelli P Zero SmarTube, TPU",
+      "Max Tire Size": "32mm",
+    },
+    components: {
+      Seatpost: "Madone Aero Carbon Seatpost, 0mm Offset, Short Length",
+      "Handlebar Tape": "Trek EcoTack",
+      Brake: "SRAM Red AXS E1 Hydraulic Disc, Flat Mount",
+      "Brake Rotor": "SRAM Paceline X, Rounded, Centerlock, 160mm",
+      "Rotor Size": "Max Brake Rotor Sizes: 180mm Front, 160mm Rear",
+    },
+  },
+
+  size_specific_specs: [
+    {
+      size: "XS",
+      specs: {
+        Crank: "SRAM RED AXS E1, Power Meter, 48/35, DUB, 165mm length",
+        Saddle: "Aeolus RSL, Carbon Rails, 155mm width",
+        "Integrated Bar Stem":
+          "Trek Aero RSL Road integrated bar/stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 35cm control width, 38cm drop width, 80mm stem length",
+      },
+    },
+    {
+      size: "S",
+      specs: {
+        Crank: "SRAM RED AXS E1, Power Meter, 48/35, DUB, 165mm length",
+        Saddle: "Aeolus RSL, Carbon Rails, 155mm width",
+        "Integrated Bar Stem":
+          "Trek Aero RSL Road integrated bar/stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 37cm control width, 40cm drop width, 90mm stem length",
+      },
+    },
+    {
+      size: "M",
+      specs: {
+        Crank: "SRAM RED AXS E1, Power Meter, 48/35, DUB, 170mm length",
+        Saddle: "Aeolus RSL, Carbon Rails, 145mm width",
+        "Integrated Bar Stem":
+          "Trek Aero RSL Road integrated bar/stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 39cm control width, 42cm drop width, 90mm stem length",
+      },
+    },
+    {
+      size: "ML",
+      specs: {
+        Crank: "SRAM RED AXS E1, Power Meter, 48/35, DUB, 170mm length",
+        Saddle: "Aeolus RSL, Carbon Rails, 145mm width",
+        "Integrated Bar Stem":
+          "Trek Aero RSL Road integrated bar/stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 39cm control width, 42cm drop width, 100mm stem length",
+      },
+    },
+    {
+      size: "L",
+      specs: {
+        Crank: "SRAM RED AXS E1, Power Meter, 48/35, DUB, 172.5mm length",
+        Saddle: "Aeolus RSL, Carbon Rails, 145mm width",
+        "Integrated Bar Stem":
+          "Trek Aero RSL Road integrated bar/stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 39cm control width, 42cm drop width, 100mm stem length",
+      },
+    },
+    {
+      size: "XL",
+      specs: {
+        Crank: "SRAM RED AXS E1, Power Meter, 48/35, DUB, 172.5mm length",
+        Saddle: "Aeolus RSL, Carbon Rails, 145mm width",
+        "Integrated Bar Stem":
+          "Trek Aero RSL Road integrated bar/stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 41cm control width, 44cm drop width, 110mm stem length",
+      },
+    },
+  ],
+};
---
+++ src/data/types.ts
@@ -0,0 +1,135 @@
+// src/data/types.ts
+
+/**
+ * Represents a single color option for a bike model,
+ * including the name and all associated image assets.
+ */
+export interface Color {
+  name: string;
+  hero: string;
+  gallery: string[];
+}
+
+/**
+ * A flat key-value store for component specifications.
+ * Used for size-specific overrides.
+ */
+export interface Spec {
+  [key: string]: string;
+}
+
+/**
+ * A categorized key-value store for base specifications.
+ * e.g., { "drivetrain": { "Shifter": "SRAM RED..." } }
+ */
+export interface BaseSpecs {
+  [category: string]: {
+    [key: string]: string;
+  };
+}
+
+/**
+ * Defines component specifications that vary by frame size.
+ */
+export interface SizeSpecificSpec {
+  size: string;
+  specs: Spec; // e.g., { "Stem": "Trek RCS Pro, 100mm", "Handlebar": "Bontrager RSL Aero, 420mm" }
+}
+
+/**
+ * Represents a specific, purchasable variant of a bike model.
+ * Each combination of size and color has a unique SKU.
+ */
+export interface Variant {
+  sku: string;
+  size: string;
+  colorName: string; // This links to a color defined in the parent Model
+  price: number;
+}
+
+/**
+ * Represents a bike model (e.g., "Madone SLR 9 AXS"), which is now a collection of variants.
+ * This level holds the specifications and color options common to all its variants.
+ */
+export interface Model {
+  modelName: string; // e.g., "SLR 9 AXS"
+  overview: string;
+  colors: Color[]; // Defines all available colors and their image assets for this model.
+  base_specs: BaseSpecs;
+  size_specific_specs: SizeSpecificSpec[];
+  variants: Variant[]; // Contains all the unique, purchasable combinations.
+}
+
+/**
+ * Represents the geometric measurements for a specific frame size.
+ * Allows for various measurements like 'stack', 'reach', etc.
+ */
+export interface Geometry {
+  size: string;
+  [key: string]: number | string;
+}
+
+/**
+ * Describes a key feature of a bike generation, like a new technology.
+ */
+export interface Feature {
+  name: string;
+  description: string;
+  image?: string; // Optional image to illustrate the feature
+}
+
+/**
+ * Represents a single service or replacement part.
+ */
+export interface ServicePart {
+  itemType: string;
+  description: string;
+  partNumber?: string;
+  manufacturerPartNumber?: string;
+  alternativeItems?: string;
+}
+
+/**
+ * Represents a range of measurements, like height or inseam, in different units.
+ */
+export interface MeasurementRange {
+  cm: string;
+  in: string;
+}
+
+/**
+ * Provides a sizing recommendation, linking a size label to rider height and inseam ranges.
+ */
+export interface SizingRecommendation {
+  size_label: string; // e.g., "XS (47cm)"
+  rider_height: MeasurementRange;
+  inseam: MeasurementRange;
+}
+
+/**
+ * Represents a specific generation of a bike family (e.g., "Madone Gen 8").
+ * Contains information shared across all models within that generation.
+ */
+export interface Generation {
+  generationName: string; // e.g., "Gen 8"
+  year: number;
+  description: string;
+  features: Feature[];
+  sizing: SizingRecommendation[];
+  geometry: Geometry[];
+  serviceParts: ServicePart[];
+  models: Model[];
+}
+
+/**
+ * The top-level object representing a whole family of bikes (e.g., "Madone").
+ * Main object, export from each data file.
+ */
+export interface BikeFamily {
+  id: string; // URL-friendly slug, e.g., "madone"
+  name: string; // e.g., "Madone"
+  category: "Road" | "Mountain" | "Gravel" | "City" | "Electric" | "Kids";
+  subCategory: string; // e.g., "Race", "Endurance", "Trail"
+  familyDescription: string;
+  generations: Generation[];
+}
