2bff925c107dae838ef1267c1ea89b001b47f657
GrungeElFz <gdelviora@0x01000111.io>
2025-08-19 22:46:48 -0500

    Added:    Product data for `Checkpoint SL 7 AXS` (src/data/bikes/gravel/checkpoint/gen-3/sl-7-axs.ts)

---
+++ src/data/bikes/gravel/checkpoint/gen-3/sl-7-axs.ts
@@ -0,0 +1,251 @@
+import type { Model } from "@/data/types";
+
+export const checkpointSL7AXS: Model = {
+  modelName: "SL 7 AXS",
+  overview:
+    "Our carbon gravel bike built to explore the beyond. Endurance geometry, tons of mounts, and massive clearance for up to 50mm tires.",
+  colors: [
+    {
+      name: "Dark Red / Carbon Red Smoke Splatter",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-26-55097-A-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-26-55097-A-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-26-55097-A-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-26-55097-A-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-26-55097-A-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-26-55097-A-Accessory1",
+      ],
+    },
+    {
+      name: "Bronze Age / Carbon Smoke Matte",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-A-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-A-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-A-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-A-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-A-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-A-Accessory1",
+      ],
+    },
+    {
+      name: "Baja Yellow Matte / Drab Olive Matte",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-B-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-B-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-B-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-B-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-B-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckpointSL7AXS-25-47073-B-Accessory1",
+      ],
+    },
+  ],
+
+  variants: [
+    // <----- Variant: Dark Red / Carbon Red Smoke Splatter ----->
+    {
+      sku: "5334683",
+      size: "XS",
+      colorName: "Dark Red / Carbon Red Smoke Splatter",
+      price: 5999.99,
+    },
+    {
+      sku: "5334684",
+      size: "S",
+      colorName: "Dark Red / Carbon Red Smoke Splatter",
+      price: 5999.99,
+    },
+    {
+      sku: "5334685",
+      size: "M",
+      colorName: "Dark Red / Carbon Red Smoke Splatter",
+      price: 5999.99,
+    },
+    {
+      sku: "5334686",
+      size: "ML",
+      colorName: "Dark Red / Carbon Red Smoke Splatter",
+      price: 5999.99,
+    },
+    {
+      sku: "5334687",
+      size: "L",
+      colorName: "Dark Red / Carbon Red Smoke Splatter",
+      price: 5999.99,
+    },
+    {
+      sku: "5334688",
+      size: "XL",
+      colorName: "Dark Red / Carbon Red Smoke Splatter",
+      price: 5999.99,
+    },
+
+    // <----- Variant: Bronze Age/Carbon Smoke Matte ----->
+    {
+      sku: "5321670",
+      size: "XS",
+      colorName: "Bronze Age/Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321671",
+      size: "S",
+      colorName: "Bronze Age/Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321672",
+      size: "M",
+      colorName: "Bronze Age/Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321673",
+      size: "ML",
+      colorName: "Bronze Age/Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321674",
+      size: "L",
+      colorName: "Bronze Age/Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321675",
+      size: "XL",
+      colorName: "Bronze Age/Carbon Smoke Matte",
+      price: 5999.97,
+    },
+
+    // <----- Variant: Bronze Age / Carbon Smoke Matte ----->
+    {
+      sku: "5321676",
+      size: "XS",
+      colorName: "Bronze Age / Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321677",
+      size: "S",
+      colorName: "Bronze Age / Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321678",
+      size: "M",
+      colorName: "Bronze Age / Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321679",
+      size: "ML",
+      colorName: "Bronze Age / Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321680",
+      size: "L",
+      colorName: "Bronze Age / Carbon Smoke Matte",
+      price: 5999.97,
+    },
+    {
+      sku: "5321681",
+      size: "XL",
+      colorName: "Bronze Age / Carbon Smoke Matte",
+      price: 5999.97,
+    },
+  ],
+
+  base_specs: {
+    frameset: {
+      Frame: "500 Series OCLV Carbon",
+      Fork: "Checkpoint SLR Tapered Carbon Steerer",
+    },
+    weight: {
+      Weight: "ML - 7.55 kg / 16.65 lbs (with TLR sealant, no tubes)",
+      "Weight Limit":
+        "125 kg / 275 lbs (Maximum Combined Weight of Bicycle, Rider, and Cargo)",
+    },
+    "e-system": {
+      Battery: "SRAM AXS battery pack (with Charger)",
+    },
+    drivetrain: {
+      Shifter: "SRAM Force AXS E1",
+      "Rear Derailleur": "SRAM Force XPLR eTap AXS, 46T Max Cog",
+      "Bottom Bracket": "SRAM DUB, T47 Threaded, Internal Bearing",
+      Cassette: "SRAM Force XPLR XG-1371, 10-46T, 13 Speed",
+      Chain: "SRAM Force E1, 12/13 Speed",
+    },
+    wheels: {
+      "Wheel - Front":
+        "Bontrager Aeolus Elite 35V, OCLV Carbon, Tubeless Ready, 35mm Rim Depth, 100x12mm Thru Axle",
+      "Wheel - Rear":
+        "Bontrager Aeolus Elite 35V, OCLV Carbon, Tubeless Ready, 35mm Rim Depth, SRAM XD-R Driver, 142x12mm Thru Axle",
+      "Skewer - Front": "Bontrager Switch Chamfered Thru Axle",
+      "Skewer - Rear": "Bontrager Switch Chamfered Thru Axle, Removable Lever",
+      Tire: "Bontrager Girona Pro, Tubeless Ready, GR puncture protection, Aramid Bead, 60 TPI, 700x42mm",
+      "Tire Part": "Bontrager TLR Sealant, 6oz",
+      "Rim Strip": "Bontrager TLR",
+      "Max Tire Size": "42mm (with fender), 50mm (without fenders)",
+    },
+    components: {
+      Saddle: "Verse Short Elite, Hollow Magnesium Rails, 145mm width",
+      Seatpost: "Bontrager carbon, 27.2mm, 8mm Offset, 330mm length",
+      "Handlebar Tape": "Bontrager Supertack Perf Tape",
+      Brake: "SRAM Force AXS E1 Hydraulic Disc, Flat Mount",
+      "Brake Rotor": "SRAM Paceline X, Rounded, Centerlock, 160mm",
+      "Rotor Size": "Max brake rotor sizes: 180mm",
+    },
+  },
+
+  size_specific_specs: [
+    {
+      size: "XS",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 165mm length",
+        Handlebar: "Bontrager Pro Gravel, 40cm width",
+        Stem: "Bontrager Pro, 31.8mm, Blendr Compatible, 7 degree, 70mm length",
+      },
+    },
+    {
+      size: "S",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 165mm length",
+        Handlebar: "Bontrager Pro Gravel, 40cm width",
+        Stem: "Bontrager Pro, 31.8mm, Blendr Compatible, 7 degree, 80mm length",
+      },
+    },
+    {
+      size: "M",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 170mm length",
+        Handlebar: "Bontrager Pro Gravel, 42cm width",
+        Stem: "Bontrager Pro, 31.8mm, Blendr Compatible, 7 degree, 80mm length",
+      },
+    },
+    {
+      size: "ML",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 170mm length",
+        Handlebar: "Bontrager Pro Gravel, 42cm width",
+        Stem: "Bontrager Pro, 31.8mm, Blendr Compatible, 7 degree, 90mm length",
+      },
+    },
+    {
+      size: "L",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 172.5mm length",
+        Handlebar: "Bontrager Pro Gravel, 44cm width",
+        Stem: "Bontrager Pro, 31.8mm, Blendr Compatible, 7 degree, 90mm length",
+      },
+    },
+    {
+      size: "XL",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 172.5mm length",
+        Handlebar: "Bontrager Pro Gravel, 46cm width",
+        Stem: "Bontrager Pro, 31.8mm, Blendr Compatible, 7 degree, 100mm length",
+      },
+    },
+  ],
+};
