19685b1acc55c5b607cae2dae4621770890d9585
GrungeElFz <gdelviora@0x01000111.io>
2025-08-19 22:27:56 -0500

    Added:    Product data for `Checkmate SLR 7 AXS` (src/data/bikes/gravel/checkmate/gen-1/slr-7-axs.ts)

---
+++ src/data/bikes/gravel/checkmate/gen-1/slr-7-axs.ts
@@ -0,0 +1,299 @@
+import type { Model } from "@/data/types";
+
+export const checkmateSLR7AXS: Model = {
+  modelName: "SLR 7 AXS",
+  overview:
+    "Our fastest and lightest gravel race bike, built to chase podiums down rowdy roads.",
+  colors: [
+    {
+      name: "Matte Trek Black / Matte Deep Smoke",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-A-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-A-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-A-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-A-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-A-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-25-46152-A-Accessory1",
+      ],
+    },
+    {
+      name: "Matte Hex Blue / Plasma Grey Pearl",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-B-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-B-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-B-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-B-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-B-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-25-46152-B-Accessory1",
+      ],
+    },
+    {
+      name: "Olive Drab / Glowstick",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-D-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-D-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-D-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-D-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-D-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-25-46152-D-Accessory1",
+      ],
+    },
+    {
+      name: "Era White / Buff Beige",
+      hero: "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-C-Primary",
+      gallery: [
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-C-Alt1",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-C-Alt2",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-C-Alt3",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-26-46152-C-Alt4",
+        "https://media.trekbikes.com/image/upload/w_1920,h_1440,c_pad,f_auto,fl_progressive:semi,q_auto/CheckmateSLR9AXS-25-46152-C-Accessory1",
+      ],
+    },
+  ],
+
+  variants: [
+    // <----- Variant: Matte Trek Black / Matte Deep Smoke ----->
+    {
+      sku: "5339967",
+      size: "XS",
+      colorName: "Matte Trek Black / Matte Deep Smoke",
+      price: 8199.99,
+    },
+    {
+      sku: "5339968",
+      size: "S",
+      colorName: "Matte Trek Black / Matte Deep Smoke",
+      price: 8199.99,
+    },
+    {
+      sku: "5339969",
+      size: "M",
+      colorName: "Matte Trek Black / Matte Deep Smoke",
+      price: 8199.99,
+    },
+    {
+      sku: "5339970",
+      size: "ML",
+      colorName: "Matte Trek Black / Matte Deep Smoke",
+      price: 8199.99,
+    },
+    {
+      sku: "5339971",
+      size: "L",
+      colorName: "Matte Trek Black / Matte Deep Smoke",
+      price: 8199.99,
+    },
+    {
+      sku: "5339972",
+      size: "XL",
+      colorName: "Matte Trek Black / Matte Deep Smoke",
+      price: 8199.99,
+    },
+
+    // <----- Variant: Matte Hex Blue / Plasma Grey Pearl ----->
+    {
+      sku: "5339973",
+      size: "XS",
+      colorName: "Matte Hex Blue / Plasma Grey Pearl",
+      price: 8199.99,
+    },
+    {
+      sku: "5339974",
+      size: "S",
+      colorName: "Matte Hex Blue / Plasma Grey Pearl",
+      price: 8199.99,
+    },
+    {
+      sku: "5339975",
+      size: "M",
+      colorName: "Matte Hex Blue / Plasma Grey Pearl",
+      price: 8199.99,
+    },
+    {
+      sku: "5339976",
+      size: "ML",
+      colorName: "Matte Hex Blue / Plasma Grey Pearl",
+      price: 8199.99,
+    },
+    {
+      sku: "5339977",
+      size: "L",
+      colorName: "Matte Hex Blue / Plasma Grey Pearl",
+      price: 8199.99,
+    },
+    {
+      sku: "5339978",
+      size: "XL",
+      colorName: "Matte Hex Blue / Plasma Grey Pearl",
+      price: 8199.99,
+    },
+
+    // <----- Variant: Olive Drab / Glowstick ----->
+    {
+      sku: "5339985",
+      size: "XS",
+      colorName: "Olive Drab / Glowstick",
+      price: 8199.99,
+    },
+    {
+      sku: "5339986",
+      size: "S",
+      colorName: "Olive Drab / Glowstick",
+      price: 8199.99,
+    },
+    {
+      sku: "5339987",
+      size: "M",
+      colorName: "Olive Drab / Glowstick",
+      price: 8199.99,
+    },
+    {
+      sku: "5339988",
+      size: "ML",
+      colorName: "Olive Drab / Glowstick",
+      price: 8199.99,
+    },
+    {
+      sku: "5339989",
+      size: "L",
+      colorName: "Olive Drab / Glowstick",
+      price: 8199.99,
+    },
+    {
+      sku: "5339990",
+      size: "XL",
+      colorName: "Olive Drab / Glowstick",
+      price: 8199.99,
+    },
+
+    // <----- Variant: Era White / Buff Beige ----->
+    {
+      sku: "5339979",
+      size: "XS",
+      colorName: "Era White / Buff Beige",
+      price: 8199.99,
+    },
+    {
+      sku: "5339980",
+      size: "S",
+      colorName: "Era White / Buff Beige",
+      price: 8199.99,
+    },
+    {
+      sku: "5339981",
+      size: "M",
+      colorName: "Era White / Buff Beige",
+      price: 8199.99,
+    },
+    {
+      sku: "5339982",
+      size: "ML",
+      colorName: "Era White / Buff Beige",
+      price: 8199.99,
+    },
+    {
+      sku: "5339983",
+      size: "L",
+      colorName: "Era White / Buff Beige",
+      price: 8199.99,
+    },
+    {
+      sku: "5339984",
+      size: "XL",
+      colorName: "Era White / Buff Beige",
+      price: 8199.99,
+    },
+  ],
+
+  base_specs: {
+    frameset: {
+      Frame: "800 Series OCLV Carbon",
+      Fork: "Checkmate SLR Tapered Carbon Steerer",
+    },
+    weight: {
+      Weight: "ML - 7.55 kg / 16.65 lbs (with TLR sealant, no tubes)",
+      "Weight Limit":
+        "125 kg / 275 lbs (Maximum Combined Weight of Bicycle, Rider, and Cargo)",
+    },
+    "e-system": {
+      Battery: "SRAM AXS battery pack (with Charger)",
+    },
+    drivetrain: {
+      Shifter: "SRAM Force AXS E1",
+      "Rear Derailleur": "SRAM Force XPLR eTap AXS, 46T Max Cog",
+      "Bottom Bracket": "SRAM DUB, T47 Threaded, Internal Bearing",
+      Cassette: "SRAM Force XPLR XG-1371, 10-46T, 13 Speed",
+      Chain: "SRAM Force E1, 12/13 Speed",
+    },
+    wheels: {
+      "Wheel - Front":
+        "Bontrager Aeolus Pro 3V, OCLV Carbon, Tubeless Ready, 25mm Rim Depth, 100x12mm Thru Axle",
+      "Wheel - Rear":
+        "Bontrager Aeolus Pro 3V, OCLV Carbon, Tubeless Ready, 25mm Rim Depth, SRAM XD-R Driver, 142x12mm Thru Axle",
+      "Skewer - Front": "Bontrager Switch Chamfered Thru Axle",
+      "Skewer - Rear": "Bontrager Switch Chamfered Thru Axle, Removable Lever",
+      Tire: "Bontrager Girona RSL GR, Tubeless Ready, Aramid Bead, 220 TPI, 700x38mm",
+      "Tire Part": "Bontrager TLR Sealant, 6oz",
+      "Rim Strip": "Bontrager TLR",
+      "Max Tire Size": "38mm (with fender), 45mm (without fenders)",
+    },
+    components: {
+      Seatpost: "KVF Aero Carbon Seatpost, 5mm Offset, 280mm length",
+      "Handlebar Tape": "Trek EcoTack",
+      Brake: "SRAM Force AXS E1 Hydraulic Disc, Flat Mount",
+      "Brake Rotor": "SRAM Paceline X, Rounded, Centerlock, 160mm",
+      "Rotor Size": "Max brake rotor sizes: 180mm",
+    },
+  },
+
+  size_specific_specs: [
+    {
+      size: "XS",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 165mm length",
+        "Integrated Bar/Stem":
+          "Trek Aero RSL Road Integrated Bar/Stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 35cm control width, 38cm drop width, 80mm stem length",
+      },
+    },
+    {
+      size: "S",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 165mm length",
+        "Integrated Bar/Stem":
+          "Trek Aero RSL Road Integrated Bar/Stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 37cm control width, 40cm drop width, 90mm stem length",
+      },
+    },
+    {
+      size: "M",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 170mm length",
+        "Integrated Bar/Stem":
+          "Trek Aero RSL Road Integrated Bar/Stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 39cm control width, 42cm drop width, 90mm stem length",
+      },
+    },
+    {
+      size: "ML",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 170mm length",
+        "Integrated Bar/Stem":
+          "Trek Aero RSL Road Integrated Bar/Stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 39cm control width, 42cm drop width, 100mm stem length",
+      },
+    },
+    {
+      size: "L",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 172.5mm length",
+        "Integrated Bar/Stem":
+          "Trek Aero RSL Road Integrated Bar/Stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 39cm control width, 42cm drop width, 100mm stem length",
+      },
+    },
+    {
+      size: "XL",
+      specs: {
+        Crank: "SRAM Force XPLR, Power Meter, 42T, DUB, 172.5mm length",
+        "Integrated Bar/Stem":
+          "Trek Aero RSL Road Integrated Bar/Stem, OCLV Carbon, Race Fit, 80mm reach, 124mm drop, 41cm control width, 44cm drop width, 110mm stem length",
+      },
+    },
+  ],
+};
